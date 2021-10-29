5b1af13a089d7f8dbdf0b1efef2eea2bc3c8cae4
edu.ministori@gmail.com <edu.ministori@gmail.com>
2021-10-29 12:20:16 +0900

    add

--- react_prj/src/AppTodo.js
+++ react_prj/src/AppTodo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 import TodoHeader from './todo/TodoHeader';
 import TodoContents from './todo/TodoContents';
@@ -9,7 +9,24 @@ import TodoItem from './todo/TodoItem';
 import TodoStatus from './todo/TodoStatus';
 import TodoFooter from './todo/TodoFooter';
 
-function App() {
+function AppTodo() {
+
+  const todoArray = [
+    {
+      id : 1,
+      content : '할일 todo 1'
+    },
+    {
+      id : 2,
+      content : '할일 todo 2'
+    },
+    {
+      id : 3,
+      content : '할일 todo 3'
+    }
+
+  ];
+
   return (
     <>
       <TodoHeader />
@@ -24,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppTodo;
